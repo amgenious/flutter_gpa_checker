4192b39d41e1f52724d0cb6b5cf8e829c05b78bc
amgenious <henrytweneboah956@gmail.com>
2024-03-08 21:52:30 +0000

    added a splash screen to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:grades_class/homepage.dart';
+// import 'package:grades_class/homepage.dart';
+import 'package:grades_class/splashscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: SplashScreen(),
     );
   }
 }
